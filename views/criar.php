2a328c1f10e1ac08cc3665a01e70f44063a86896
haradadev <akira@institutocory.com.br>
2026-02-27 12:24:37 -0300

    first commit

---
+++ views/criar.php
@@ -0,0 +1,11 @@
+<h2>Novo Usuário</h2>
+
+<form method="POST" enctype="multipart/form-data">
+
+Nome: <input type="text" name="nome"><br><br>
+Email: <input type="text" name="email"><br><br>
+Imagem: <input type="file" name="imagem"><br><br>
+
+<button type="submit">Salvar</button>
+
+</form>
---
+++ views/editar.php
@@ -0,0 +1,19 @@
+<h2>Editar Usuário</h2>
+<form method="POST" enctype="multipart/form-data">
+Nome:
+<input type="text" name="nome" value="<?php echo $usuario['nome']; ?>"><br><br>
+Email:
+<input type="text" name="email" value="<?php echo $usuario['email']; ?>"><br><br>
+<?php if ($usuario['imagem']) { ?>
+    <img src="<?php echo $usuario['imagem']; ?>" width="100"><br>
+
+    <?php } ?>
+
+Nova Imagem:
+<input type="file" name="imagem"><br><br>
+
+<input type="hidden" name="imagem_atual" value="<?php echo $usuario['imagem']; ?>">
+
+<button type="submit">Atualizar</button>
+
+</form>
